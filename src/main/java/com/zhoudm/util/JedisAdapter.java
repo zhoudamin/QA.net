dfffd1f66d041e518cf93292b8562f6af3f5f173
DarminZhou <790471332@qq.com>
2017-08-17 15:17:59 +0800

    完善下

--- src/main/java/com/zhoudm/util/JedisAdapter.java
+++ src/main/java/com/zhoudm/util/JedisAdapter.java
@@ -27,125 +27,9 @@ public class JedisAdapter implements InitializingBean {
     }
 
     public static void mainx(String[] argv) {
-        Jedis jedis = new Jedis("redis://localhost:6379/9");
-        jedis.flushDB();
+        Jedis jedis = new Jedis("redis://localhost:6379/9"); //连到第9个库
+        jedis.flushDB(); //删掉
 
-        // get set
-        jedis.set("hello", "world");
-        print(1, jedis.get("hello"));
-        jedis.rename("hello", "newhello");
-        print(1, jedis.get("newhello"));
-        jedis.setex("hello2", 1800, "world");
-
-        //
-        jedis.set("pv", "100");
-        jedis.incr("pv");
-        jedis.incrBy("pv", 5);
-        print(2, jedis.get("pv"));
-        jedis.decrBy("pv", 2);
-        print(2, jedis.get("pv"));
-
-        print(3, jedis.keys("*"));
-
-        String listName = "list";
-        jedis.del(listName);
-        for (int i = 0; i < 10; ++i) {
-            jedis.lpush(listName, "a" + String.valueOf(i));
-        }
-        print(4, jedis.lrange(listName, 0, 12));
-        print(4, jedis.lrange(listName, 0, 3));
-        print(5, jedis.llen(listName));
-        print(6, jedis.lpop(listName));
-        print(7, jedis.llen(listName));
-        print(8, jedis.lrange(listName, 2, 6));
-        print(9, jedis.lindex(listName, 3));
-        print(10, jedis.linsert(listName, BinaryClient.LIST_POSITION.AFTER, "a4", "xx"));
-        print(10, jedis.linsert(listName, BinaryClient.LIST_POSITION.BEFORE, "a4", "bb"));
-        print(11, jedis.lrange(listName, 0 ,12));
-
-        // hash
-        String userKey = "userxx";
-        jedis.hset(userKey, "name", "jim");
-        jedis.hset(userKey, "age", "12");
-        jedis.hset(userKey, "phone", "18618181818");
-        print(12, jedis.hget(userKey, "name"));
-        print(13, jedis.hgetAll(userKey));
-        jedis.hdel(userKey, "phone");
-        print(14, jedis.hgetAll(userKey));
-        print(15, jedis.hexists(userKey, "email"));
-        print(16, jedis.hexists(userKey, "age"));
-        print(17, jedis.hkeys(userKey));
-        print(18, jedis.hvals(userKey));
-        jedis.hsetnx(userKey, "school", "zju");
-        jedis.hsetnx(userKey, "name", "yxy");
-        print(19, jedis.hgetAll(userKey));
-
-        // set
-        String likeKey1 = "commentLike1";
-        String likeKey2 = "commentLike2";
-        for (int i = 0; i < 10; ++i) {
-            jedis.sadd(likeKey1, String.valueOf(i));
-            jedis.sadd(likeKey2, String.valueOf(i*i));
-        }
-        print(20, jedis.smembers(likeKey1));
-        print(21, jedis.smembers(likeKey2));
-        print(22, jedis.sunion(likeKey1, likeKey2));
-        print(23, jedis.sdiff(likeKey1, likeKey2));
-        print(24, jedis.sinter(likeKey1, likeKey2));
-        print(25, jedis.sismember(likeKey1, "12"));
-        print(26, jedis.sismember(likeKey2, "16"));
-        jedis.srem(likeKey1, "5");
-        print(27, jedis.smembers(likeKey1));
-        jedis.smove(likeKey2, likeKey1, "25");
-        print(28, jedis.smembers(likeKey1));
-        print(29, jedis.scard(likeKey1));
-
-        String rankKey = "rankKey";
-        jedis.zadd(rankKey, 15, "jim");
-        jedis.zadd(rankKey, 60, "Ben");
-        jedis.zadd(rankKey, 90, "Lee");
-        jedis.zadd(rankKey, 75, "Lucy");
-        jedis.zadd(rankKey, 80, "Mei");
-        print(30, jedis.zcard(rankKey));
-        print(31, jedis.zcount(rankKey, 61, 100));
-        print(32, jedis.zscore(rankKey, "Lucy"));
-        jedis.zincrby(rankKey, 2, "Lucy");
-        print(33, jedis.zscore(rankKey, "Lucy"));
-        jedis.zincrby(rankKey, 2, "Luc");
-        print(34, jedis.zscore(rankKey, "Luc"));
-        print(35, jedis.zrange(rankKey, 0, 100));
-        print(36, jedis.zrange(rankKey, 0, 10));
-        print(36, jedis.zrange(rankKey, 1, 3));
-        print(36, jedis.zrevrange(rankKey, 1, 3));
-        for (Tuple tuple : jedis.zrangeByScoreWithScores(rankKey, "60", "100")) {
-            print(37, tuple.getElement() + ":" + String.valueOf(tuple.getScore()));
-        }
-
-        print(38, jedis.zrank(rankKey, "Ben"));
-        print(39, jedis.zrevrank(rankKey, "Ben"));
-
-        String setKey = "zset";
-        jedis.zadd(setKey, 1, "a");
-        jedis.zadd(setKey, 1, "b");
-        jedis.zadd(setKey, 1, "c");
-        jedis.zadd(setKey, 1, "d");
-        jedis.zadd(setKey, 1, "e");
-
-        print(40, jedis.zlexcount(setKey, "-", "+"));
-        print(41, jedis.zlexcount(setKey, "(b", "[d"));
-        print(42, jedis.zlexcount(setKey, "[b", "[d"));
-        jedis.zrem(setKey, "b");
-        print(43, jedis.zrange(setKey, 0, 10));
-        jedis.zremrangeByLex(setKey, "(c", "+");
-        print(44, jedis.zrange(setKey, 0 ,2));
-
-        /*
-        JedisPool pool = new JedisPool();
-        for (int i = 0; i < 100; ++i) {
-            Jedis j = pool.getResource();
-            print(45, j.get("pv"));
-            j.close();
-        }*/
 
         User user = new User();
         user.setName("xx");
@@ -182,6 +66,7 @@ public class JedisAdapter implements InitializingBean {
         pool = new JedisPool("redis://localhost:6379/10");
     }
 
+    //添加
     public long sadd(String key, String value) {
         Jedis jedis = null;
         try {
@@ -197,6 +82,8 @@ public class JedisAdapter implements InitializingBean {
         return 0;
     }
 
+
+//减去
     public long srem(String key, String value) {
         Jedis jedis = null;
         try {
@@ -212,6 +99,7 @@ public class JedisAdapter implements InitializingBean {
         return 0;
     }
 
+    //数量
     public long scard(String key) {
         Jedis jedis = null;
         try {
@@ -227,6 +115,7 @@ public class JedisAdapter implements InitializingBean {
         return 0;
     }
 
+    //是否成员
     public boolean sismember(String key, String value) {
         Jedis jedis = null;
         try {
